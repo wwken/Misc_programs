b8751f85dc4fc1bdf1a0566571764526884afa8b
Ken W <ken@lG-Q>
2014-11-19 21:34:45 -0500

    Neo4jDataImport v1

---
+++ neo4jDataImport/src/main/java/com/wwken/neo4jdataimport/domain/WikiRelationship.java
@@ -0,0 +1,12 @@
+/*
+ * 
+ * Copyright (c) 2014 Ken Wu
+ * 
+ */
+
+
+package com.wwken.neo4jdataimport.domain;
+
+import org.neo4j.graphdb.RelationshipType;
+
+public enum WikiRelationship implements RelationshipType { Link }
