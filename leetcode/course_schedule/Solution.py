506d4feda5db7675d36af7db2200060260eebe32
Ken Wu <ken_11223@yahoo.com>
2018-11-01 18:28:22 -0400

    leetcode - course schedule 2

--- leetcode/course_schedule/Solution.py
+++ leetcode/course_schedule/Solution.py
@@ -1,70 +1,32 @@
 class Solution(object):
-    def testCycles(self, pre, startingCourse, currentCourse, wholeRoutes, allFinalCycles):
-        if startingCourse == currentCourse:
-            # print("There is a cycles....")
-            for c in wholeRoutes:
-                allFinalCycles.add(c)
-            return True
-        if currentCourse == None:
-            currentCourse = startingCourse
-        if currentCourse not in pre:
-            return False
-        if currentCourse in wholeRoutes:
-            # print("There is a cycles....")
-            for c in wholeRoutes:
-                allFinalCycles.add(c)
-            return True
-        else:
-            wholeRoutes.add(currentCourse)
-        allPres = pre[currentCourse]
-
-        isThereCycles = False
-        for p in allPres:
-            # now try to see if there is cycles
-            thisCycles = self.testCycles(pre, startingCourse, p, wholeRoutes.copy(), allFinalCycles)
-            isThereCycles = isThereCycles or thisCycles
-        return isThereCycles
-
-
     def canFinish(self, numCourses, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        pre = {}
+        class DiGraph:
+            def __init__(self, prereqs):
+                self.data = {}
+                for v, k in prereqs:
+                    if k not in self.data:
+                        self.data[k] = []
+                    self.data[k].append(v)
 
-        # build the prerequisites
-        for e in prerequisites:
-            c = e[0]
-            p = e[1]
-            if c in pre:
-                pre[c].append(p)
-            else:
-                pre[c] = [p]
+            def is_cyclic(self):
+                path = set()
+                visited = set()
 
-        allFinalCycles = set()
-        for c in pre:
-            self.testCycles(pre, c, None, set(), allFinalCycles)
+                def visit_vertex(v):
+                    if v in visited:
+                        return False
+                    path.add(v)
+                    visited.add(v)
+                    for neighbor in self.data.get(v, ()):
+                        if neighbor in path or visit_vertex(neighbor):
+                            return True
+                    path.remove(v)
+                    return False
+                return any(visit_vertex(v) for v in self.data)
 
-        if len(allFinalCycles):
-            # There is cycles
-            for c in allFinalCycles:
-                pre.pop(c, None)
-
-        totalCoursesCount = len(pre)
-
-        if totalCoursesCount == 1:
-            totalCoursesCount = 2
-
-        if numCourses <= totalCoursesCount:
-            # print("numCourses <= totalCoursesCount, Good")
-            return True
-        else:
-            # print("numCourses > totalCoursesCount, bad")
-            return False
-
-
-
-
-
+        return not DiGraph(prerequisites).is_cyclic()
