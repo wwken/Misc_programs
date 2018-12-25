52e3fbdf7353fcf6578ea8a18c0ff8fb58929094
Ken Wu <ken_11223@yahoo.com>
2018-12-24 23:57:20 -0500

    spark problems - treeset

---
+++ BigDataProblems/Spark-process-nyc-tree-set-statistics/README.md
@@ -0,0 +1,6 @@
+This PySpark job is to take the input data from: https://data.cityofnewyork.us/Environment/2015-Street-Tree-Census-Tree-Data/pi5s-9p35 
+
+and group by date and block id to find all distinct spc_common names
+
+The screenshot can be referred in the screenshot folder
+
---
+++ BigDataProblems/Spark-process-nyc-tree-set-statistics/processTree.py
@@ -0,0 +1,37 @@
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from functools import reduce
+import pandas as pd
+import numpy as np
+from pyspark.sql.functions import col, collect_list, udf
+from pyspark.sql.types import StringType
+
+# This PySpark job is to take the input data from: https://data.cityofnewyork.us/Environment/2015-Street-Tree-Census-Tree-Data/pi5s-9p35 and group by date and block id to find the individual sum
+
+sc = SparkContext('local','example')  # if using locally
+sql_sc = SQLContext(sc)
+
+input_file = './2015StreetTreesCensus_TREES.csv'
+# input_file = './2015StreetTreesCensus_TREES_small.csv'
+pandas_df = pd.read_csv(input_file, sep=",", delimiter=",")  # assuming the file contains a header
+pandas_df = pandas_df.replace(np.nan, '', regex=True)   # there are some empty columns (internally interpreted as nan inside dataframe) in the file that caused exceptions so need to map it to an empty string
+pandas_df = pandas_df[['created_at', 'block_id', 'spc_common']]
+
+pandas_df = sql_sc.createDataFrame(pandas_df)
+
+created_at_block_id_summaries = pandas_df.groupby(['created_at', 'block_id'])
+
+
+# Use udf to define a row-at-a-time udf
+def myFunc(data_list):
+    count = len(data_list)
+    output = reduce(lambda x, y: x + ',' + y, data_list)
+    return "{} {}".format(count, output)
+
+myUdf = udf(myFunc, StringType())
+
+created_at_block_id_summaries = created_at_block_id_summaries.agg(collect_list('spc_common').alias('spc_common')).withColumn('spc_common', myUdf('spc_common'))
+
+# created_at_block_id_summaries.head(50)  # take the first 100 rows
+
+created_at_block_id_summaries.show()
